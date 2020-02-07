4aa956a6310b6b35464cc23ebcf8ac8e8edd2033
Robson Silva <robsonads@outlook.com>
2020-02-06 21:36:19 -0300

    Update Kruskal.cpp
    Change @brief.

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -2,8 +2,8 @@
 /**
  * @algorithm - Kruskal algorithm 
  * @brief Arvore Geradora Minima - Kruskal.
- *        Dando um gafo G(E), não direcionado
- *        e ponderado. Retornar o caminho mais
+ *        Dado um gafo G(E), não direcionado
+ *        e ponderado. Retornar o caminho 
  *        mais curto com base nas ponderações.
  *
  * @link - https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
